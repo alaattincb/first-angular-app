3c3281b2776b2d580090adfee80cc61e97164bfa
alaattincb <cobanoglualaattin@gmail.com>
2024-07-20 18:06:54 +0300

    cli ile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from "./movie/movie.component";
 
 @NgModule({
   declarations: [
@@ -15,8 +16,9 @@ import { MoviesComponent } from './movies/movies.component';
     BrowserModule,
     CommonModule,
     FormsModule,
-    RouterModule.forRoot([])
-  ],
+    RouterModule.forRoot([]),
+    MovieComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
